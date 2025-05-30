65524e3aa4b29c82628752d7e42a78530c610a2e
nullsploit01 <harshaldharmik099@gmail.com>
2025-05-30 21:19:45 +0530

    add base shaders

--- src/pages/library/loading/component.tsx
+++ src/pages/library/loading/component.tsx
@@ -1,28 +1,32 @@
+import fragmentShader from './shaders/fragment.glsl';
+import vertexShader from './shaders/vertex.glsl';
 import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
-import { DoubleSide, type Mesh, type MeshStandardMaterial } from 'three';
+import { DoubleSide, type Mesh } from 'three';
 
 import { useGuiControls } from '@/hooks/use-gui-controls';
 
 const LoadingComponent = () => {
   const meshRef = useRef<Mesh>(null);
-  const params = useRef({ speed: 0, color: '#8AC8AC' }).current;
+  const params = useRef({ speed: 0 }).current;
 
   useGuiControls('Box Controls', params, [
     { property: 'speed', type: 'number', options: { min: 0, max: 0.2, step: 0.001 } },
-    { property: 'color', type: 'color' },
   ]);
 
   useFrame(() => {
     if (!meshRef.current) return;
     meshRef.current.rotation.y += params.speed;
-    (meshRef.current.material as MeshStandardMaterial).color.set(params.color);
   });
 
   return (
     <mesh ref={meshRef}>
       <planeGeometry args={[1, 1]} />
-      <meshStandardMaterial side={DoubleSide} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        side={DoubleSide}
+      />
       <ambientLight intensity={2} />
     </mesh>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), glsl()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
