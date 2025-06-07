4b2ff6d19545c765067d56526a08b647abde5f64
nullsploit01 <harshaldharmik099@gmail.com>
2025-06-07 15:31:25 +0530

    add a streep lamp

--- src/pages/library/loading/component.tsx
+++ src/pages/library/loading/component.tsx
@@ -1,5 +1,6 @@
 import fragmentShader from './shaders/fragment.glsl';
 import vertexShader from './shaders/vertex.glsl';
+import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import { DoubleSide, type Mesh, ShaderMaterial, Uniform } from 'three';
@@ -9,6 +10,13 @@ import { useGuiControls } from '@/hooks/use-gui-controls';
 const LoadingComponent = () => {
   const meshRef = useRef<Mesh>(null);
   const params = useRef({ speed: 0 }).current;
+
+  const gltf = useGLTF('/models/street_lamp_01_1k.gltf/street_lamp_01_1k.gltf');
+  if (gltf.scene) {
+    gltf.scene.rotation.set(0, 1, 0);
+    gltf.scene.position.y = -20;
+    gltf.scene.position.x = -5;
+  }
 
   useGuiControls('Box Controls', params, [
     { property: 'speed', type: 'number', options: { min: 0, max: 0.2, step: 0.001 } },
@@ -24,18 +32,10 @@ const LoadingComponent = () => {
   });
 
   return (
-    <mesh ref={meshRef} position={[0, -10, 0]} rotation-z={Math.PI / 2}>
-      <points />
-      <shaderMaterial
-        fragmentShader={fragmentShader}
-        vertexShader={vertexShader}
-        side={DoubleSide}
-        transparent={true}
-        depthWrite={false}
-        uniforms={{ uTime: new Uniform(0) }}
-      />
-      <ambientLight intensity={2} />
-    </mesh>
+    <>
+      <ambientLight intensity={20} />
+      <primitive object={gltf.scene} scale={7.5} />
+    </>
   );
 };
 
