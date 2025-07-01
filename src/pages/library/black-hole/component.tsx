80ea4b13fa1df97f9f4625d3ae186272b358d3ee
nullsploit01 <harshaldharmik099@gmail.com>
2025-07-01 09:21:32 +0530

    set base shaaders for shadow and disk

--- src/pages/library/black-hole/component.tsx
+++ src/pages/library/black-hole/component.tsx
@@ -1,12 +1,31 @@
 import BlackHoleBackground from './background';
+import accretionDiskFragmentShader from './shaders/accretion-disk/fragment.glsl';
+import accretionDiskVertexShader from './shaders/accretion-disk/vertex.glsl';
+import shadowFragmentShader from './shaders/shadow/fragment.glsl';
+import shadowVertexShader from './shaders/shadow/vertex.glsl';
+import { DoubleSide } from 'three';
 
 const BlackHoleComponent = () => {
   return (
-    <group>
+    <group position-y={-1} rotation-y={3}>
       <BlackHoleBackground />
+      // accretion disk
+      <mesh rotation-x={-Math.PI / 2}>
+        <ringGeometry args={[3, 4, 64, 10]} />
+        <shaderMaterial
+          vertexShader={accretionDiskVertexShader}
+          fragmentShader={accretionDiskFragmentShader}
+          side={DoubleSide}
+        />
+      </mesh>
+      // shadow
       <mesh>
-        <sphereGeometry args={[1, 32, 32]} />
-        <meshNormalMaterial />
+        <sphereGeometry args={[2, 32, 32]} />
+        <shaderMaterial
+          vertexShader={shadowVertexShader}
+          fragmentShader={shadowFragmentShader}
+          side={DoubleSide}
+        />
       </mesh>
     </group>
   );
---
+++ src/pages/library/black-hole/shaders/shadow/fragment.glsl
@@ -0,0 +1,7 @@
+void main()
+{
+    gl_FragColor = vec4(1.0, 1.0, 0.1, 1.0);
+
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
